4c98687614e69b71fc0b30ad43e33988ad8a722d
Laurent Nocca <lnocca@fanreact.com>
2018-05-02 13:10:47 -0500

    add GET /todos route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose');
 
-// use built-in promise library
+// use built-in promise library instead of callbacks.
+// so we tell mongoose which promise library to use (built-in one)
 mongoose.Promise = global.Promise;
+// connect to local database
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-// mongoose waits for connection before saving
-
+// mongoose waits for connection before saving. no need for callback like with MongoClient in playground connect file
+// no need to micromanage the order of things
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ var app = express();
 // return value of bodyParser.json is the midleware we need to give to epxress
 app.use(bodyParser.json());
 
+// SAVE
 app.post('/todos', (req, res)=> {
   var todo = new Todo({
     text: req.body.text
@@ -22,6 +23,14 @@ app.post('/todos', (req, res)=> {
   });
 });
 
+// GET
+app.get('/todos', (req, res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos})
+  }, (e)=>{
+    res.status(400).send(e);
+  })
+});
 
 app.listen(3000, ()=>{
   console.log('Started on port 3000');
