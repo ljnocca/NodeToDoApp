ac5e25350da667b660e0a4effd0954814841b2ab
Laurent Nocca <lnocca@fanreact.com>
2018-05-02 13:17:45 -0500

    added tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,18 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text: 'first test todo'
+}, {
+  text: 'second test todo'
+}];
+
 // runs before every testcase and only moves on to test case once we call done
 beforeEach((done) => {
-  // wipe todos
-  Todo.remove({}).then(()=>done());
+  // wipe todos then insert the 2 test todos
+  Todo.remove({}).then(()=> {
+    return Todo.insertMany(todos);
+  }).then(()=> done());
 });
 
 // Use describe to group all the routes
@@ -29,7 +37,7 @@ describe('POST /todos', ()=>{
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -48,9 +56,21 @@ describe('POST /todos', ()=>{
         }
 
         Todo.find().then((todos)=>{
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e)=>done(e));
       })
+  })
+});
+
+describe('GET /todos', ()=>{
+  it('should get all todos', (done)=>{
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   })
 });
